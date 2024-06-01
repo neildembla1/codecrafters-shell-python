03dc42f569be9c38d19111e359da6e1ac32d79c4
Neil Dembla <neildembla1@gmail.com>
2024-06-01 13:10:56 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,12 +2,13 @@ import sys
 
 
 def main():
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
 
-    # Wait for user input
-    command = input()
-    sys.stdout.write(f"{command}: command not found\n")
+        # Wait for user input
+        command = input()
+        sys.stdout.write(f"{command}: command not found\n")
 
 
 if __name__ == "__main__":
